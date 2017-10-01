7bd0181ca9df17242809d0f56a01cc24325a1b51
pkbanks <pkbanks@gmail.com>
2017-10-01 14:53:54 -0400

    made 2 helper functions to grab spot prices, one is conventional, other gathers multiple quotes per base_currency

--- helpers.rb
+++ helpers.rb
@@ -1,4 +1,8 @@
 require 'date'
+require 'httparty'
+
+# references:
+# https://www.cryptocompare.com/api/#-api-data-price-
 
 def getDateFromUnixDate(unixDate)
 	# converts unixDate (String) and returns a friendly date (String)
@@ -6,12 +10,27 @@ def getDateFromUnixDate(unixDate)
 	return date.strftime('%a, %b %d, %Y')
 end
 
-def coint_spot_price(fromSym, toSyms)
+def coin_spot_price(fromSym, *toSyms)
 	# fromSym: from symbol, as String
 	# toSyms: to symbol, as multiple Strings
+	# returns a hash / object literal, where each key (String)
+	# returns a value that is equal to the number of fromSym per toSym
 
 	# example, to go from US Dollars, to Bitcoin, Ethereum, and LiteCoint:
 	# coin_spot_price('USD', 'BTC', 'ETH', 'LTC')
-	# returns 
+	
+	endpoint = "https://min-api.cryptocompare.com/data/price?"
+	url = endpoint + "fsym=" + fromSym + "&tsyms=" + toSyms.join(",")
+
+	return HTTParty.get(url).parsed_response
+
 end
 
+def prices(base_currency, *coins)
+	hash_response = coin_spot_price(base_currency, coins)
+	result = {}
+	hash_response.each do |coin, val|
+		result[coin] = 1/val 
+	end
+	result
+end
