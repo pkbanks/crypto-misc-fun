145f9d4c5779446dd2b398d6675b6bd59e2b44b9
Alex Jean-Baptiste <alxjeanbaptiste@gmail.com>
2017-10-01 14:55:46 -0400

    found the frequency of the historical api call, every 3 days is the range

--- helpers.rb
+++ helpers.rb
@@ -1,5 +1,17 @@
 require 'date'
 
+require 'nokogiri'
+require 'HTTParty'
+
+response = HTTParty.get("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=60&aggregate=3&e=CCCAGG")
+
+# puts response["Data"]
+
+# response["Data"].each do |x|
+# 	puts getDateFromUnixDate(x[:time])
+# end
+
+
 def getDateFromUnixDate(unixDate)
 	# converts unixDate (String) and returns a friendly date (String)
 	date = DateTime.strptime(unixDate, '%s')
@@ -12,6 +24,12 @@ def coint_spot_price(fromSym, toSyms)
 
 	# example, to go from US Dollars, to Bitcoin, Ethereum, and LiteCoint:
 	# coin_spot_price('USD', 'BTC', 'ETH', 'LTC')
-	# returns 
+	# returns
 end
 
+#API returns the prices for every three days
+# response["Data"].each do |x|
+# 	puts getDateFromUnixDate(x["time"].to_s)
+# end
+#
+# puts response["Data"]
